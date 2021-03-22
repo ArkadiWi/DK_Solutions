d4c8402df63af1d7e69f5d8fd849ddfb91b90702
Arkadiusz Wieczorkowski <arkadi.wi@gmail.com>
2021-03-22 19:04:30 +0100

    done

--- 1.py
+++ 1.py
@@ -10,12 +10,18 @@ else:
     print('Nie jest podzielna przez 5 i 11')
 
 print('\n\tPodaj współrzędne wierzchołków trójkąta')
-top_a1 = int(input('Wierzchołek A1: '))
-top_a2 = int(input('Wierzchołek A2: '))
-top_b1 = int(input('Wierzchołek B1: '))
-top_b2 = int(input('Wierzchołek B2: '))
-top_c1 = int(input('Wierzchołek C1: '))
+ax = float(input('Wierzchołek ax: '))
+ay = float(input('Wierzchołek ay: '))
+bx = float(input('Wierzchołek bx: '))
+by = float(input('Wierzchołek by: '))
+cx = float(input('Wierzchołek cx: '))
+cy = float(input('Wierzchołek cy: '))
 
 
-top_a1 = int(input('Wierzchołek A1: '))
-top_a2 = int(input('Wierzchołek A2: '))
+sec_AB = ((bx-ax)**2 + (by-ay)**2)**0.5
+sec_BC = ((cx-bx)**2 + (cy-by)**2)**0.5
+sec_AC = ((cx-ax)**2 + (cy-ay)**2)**0.5
+circuit = sec_AB + sec_BC + sec_AC
+print(f'Obwód trójkąta wynosi: {circuit}')
+area_of_triangle = 0.5*((bx-ax)*(cy-ay)-(by-ay)*(cx-ax))
+print(f'Pole trókąta wynosi: {abs(area_of_triangle)}')
